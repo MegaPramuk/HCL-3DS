6bdbf272e0e28f7d8c6086695799366962f96082
MegaPramuk <pascalduszczak@web.de>
2018-11-12 13:22:57 +0100

    Perform make clean
    So that it can be built on any computer

--- build/qda.d
+++
@@ -1,4 +0,0 @@
-qda.o: /home/pochodnia/3dsdev/Hydra-Castle-Labyrinth/source/qda.c \
- /home/pochodnia/3dsdev/Hydra-Castle-Labyrinth/source/qda.h
-
-/home/pochodnia/3dsdev/Hydra-Castle-Labyrinth/source/qda.h:
--- build/stagedata.d
+++
@@ -1,5 +0,0 @@
-stagedata.o: \
- /home/pochodnia/3dsdev/Hydra-Castle-Labyrinth/source/stagedata.c \
- /home/pochodnia/3dsdev/Hydra-Castle-Labyrinth/source/stagedata.h
-
-/home/pochodnia/3dsdev/Hydra-Castle-Labyrinth/source/stagedata.h:
